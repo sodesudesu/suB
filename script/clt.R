b44a4e77490575228a7552e8d670d8250877a342
taiyo <aa21049@shibaura-it.ac.jp>
2023-11-24 15:14:16 +0900

    確認

--- script/clt.R
+++ script/clt.R
@@ -16,6 +16,7 @@ dev.off()
 x = sample(1:6, 100000, replace=TRUE) # 10万個の乱数
 y = sample(1:6, 100000, replace = TRUE) # 10万個の乱数
 ave = (x + y) / 2
+
 png("../fig/2x10.png", width=640, height=480, type="cairo")
 hist(ave, breaks = 0.5:6.5,
      xlab = "出た目の平均値", ylab = "頻度", main = "")
@@ -26,20 +27,19 @@ x = sample(1:6, 100000, replace=TRUE) # 10万個の乱数
 y = sample(1:6, 100000, replace = TRUE) # 10万個の乱数
 z = sample(1:6, 100000, replace = TRUE) # 10万個の乱数
 ave = (x + y + z) / 3
+
 png("../fig/3x10.png", width=640, height=480, type="cairo")
 hist(ave, breaks = 0.5:6.5,
      xlab = "出た目の平均値", ylab = "頻度", main = "")
 dev.off()
 
-### サイコロn個をm回投げる。
-#n = 100000 # 標本数
-#m = 100000 # 各標本の要素数
+### サイコロ100個を100000回投げる。
+x = 0
+for (i in 1:100){
+    x = x + sample(1:6, 100000, replace=TRUE)
+}
 
-#y = 0
-#for (i in 1:n){
-#    y = y + sample(1:6, n, replace=TRUE)
-#}
-
-# ヒストグラムを作成
-#hist(y / n, breaks = seq(min(y/n), max(y/n), length.out = 100), freq = FALSE)
-
+png("../fig/100x10.png", width=640, height=480, type="cairo")
+hist(x/100, breaks = seq(min(x/100), max(x/100), length.out = 50),
+     xlab="出た目の平均値", ylab="頻度", main="")
+dev.off()
