e42555566a6800ccf4018a201013886ba09e67a5
taiyo <aa21049@shibaura-it.ac.jp>
2023-12-09 21:12:45 +0900

    hoge

--- script/pisa.R
+++ script/pisa.R
@@ -2,15 +2,21 @@ rm(list=ls())
 
 # データの読み込み
 library(haven)
-data = read_sav("../data/STU_QQQ_SPSS/CY08MSP_STU_QQQ.sav")
+data = read_sav("../data/CY08MSP_STU_QQQ.sav")
 
 # データサイズ表示
 object.size(data)
 
 # データの様子
 summary(data)
+
 dim(data) # サイズ
+
 names(data) # 項目
 
-# 家にデスクトップPCがあるか
-table(data$IC001Q01TA)
+plot(data$IC170Q01JA, data$IC170Q02JA)
+
+x = table(data$CNT, data$IC170Q01JA)
+head(x)
+
+barplot(sort(x[,1] / rowSums(x[,1:2])), horiz=TRUE, las=1)
